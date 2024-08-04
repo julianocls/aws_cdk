21faed442481bdecd4ba0ababb2d6b023791edba
Juliano C L Santos <foxports@hotmail.com>
2024-08-04 00:03:33 -0300

    Criando o cluster

--- src/main/java/com/myorg/AwsCdkStack.java
+++ src/main/java/com/myorg/AwsCdkStack.java
@@ -3,8 +3,6 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class AwsCdkStack extends Stack {
     public AwsCdkStack(final Construct scope, final String id) {
@@ -16,9 +14,5 @@ public class AwsCdkStack extends Stack {
 
         // The code that defines your stack goes here
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
     }
 }
